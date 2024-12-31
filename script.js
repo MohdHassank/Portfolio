aeb156d33b7e7cf932e6008d32ea9460ea32af23
Hassan khan <mohdhassankhan486@gmail.com>
2024-12-31 17:23:11 +0530

    Updated changes

---
+++ script.js
@@ -0,0 +1,20 @@
+// Ensure Typed.js is loaded before using it
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".typing", {
+      strings: [
+        "Problem Solving",
+        "Web Development",
+        "Lifelong Learnings",
+        "Designing"
+      ],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+    });
+  });
+  
+
+
+  document.getElementById('scrollButton').addEventListener('click', function() {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+});
